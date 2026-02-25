e811d2c4cb861001558e35e398ec26b50336eb68
anhnhu <anhnt@konistudio.xyz>
2026-02-25 11:18:35 +0700

    actiom12

---
+++ open-next.config.ts
@@ -0,0 +1,9 @@
+import { defineExample } from "@opennextjs/cloudflare";
+
+export default {
+  default: {
+    runtime: "cloudflare-nodejs",
+  },
+} satisfies never;
+// Lưu ý: Tùy phiên bản @opennextjs/cloudflare, bạn có thể cần điều chỉnh template
+// nhưng cấu hình trên là cơ bản nhất để chạy chế độ nodejs_compat.
